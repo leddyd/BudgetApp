9990345c1ae0929a0002d4a5dde574d04f21f11c
Dylan Leddy <leddyd@bc.edu>
2023-12-11 13:42:12 -0500

    Added text fade in, signup form functionality

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,36 +1,114 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 function RenderHome() {
     const cards = [
-        {"header": "Header", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
-        {"header": "Header", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
-        {"header": "Header", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
+        {"header": "TRACK EXPENSES", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
+        {"header": "CREATE A PLAN", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
+        {"header": "PAY LESS", "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus pretium quam vulputate dignissim suspendisse in est ante. Blandit massa enim nec dui nunc mattis enim. Odio euismod lacinia at quis risus sed. Augue mauris augue neque gravida in fermentum et. Faucibus vitae aliquet nec ullamcorper sit amet risus. Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Sit amet purus gravida quis blandit turpis cursus. Lacinia at quis risus sed vulputate odio. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu cursus euismod quis viverra nibh. Integer eget aliquet nibh praesent. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet aliquam id diam maecenas ultricies mi. Vitae justo eget magna fermentum iaculis eu non diam. Lacus sed turpis tincidunt id aliquet risus feugiat. Lectus sit amet est placerat in egestas erat."},
     ]
+    
+    const cardRefs = cards.map(() => useRef<HTMLDivElement>(null))
+
+    const handleIntersection = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+        entries.forEach((entry, index) => {
+            if (entry.isIntersecting && cardRefs[index].current) {
+                entry.target.classList.add('fade-in')
+                observer.unobserve(entry.target)
+            }
+        });
+    };
+
+    useEffect(() => {
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: .25
+        };
+
+        const observer = new IntersectionObserver(handleIntersection, observerOptions)
+
+        cardRefs.forEach((ref) => {
+            if (ref.current) {
+                observer.observe(ref.current)
+            }
+        });
+
+        return () => {
+            observer.disconnect()
+        };
+    }, [cardRefs]);
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        console.log({firstName, lastName, email, password, confirmPassword});
+    };
 
     return (
         <div id="home">
             <div id="home-header-container">
                 <div>
-                    <p>SUB HEADER</p>
-                    <h1>THIS IS THE HEADER</h1>
+                    <p>PERSONAL FINANCE WITH WEGONBUDGET</p>
+                    <h1>MAKE A CHANGE</h1>
                 </div>
             </div>
             <div id="home-content-container">
                 <div id="home-text-container">
-                    {cards.map((card) => (
-                        <div className="home-card">
+                    {cards.map((card, index) => (
+                        <div className="home-card" key={index} ref={cardRefs[index]}>
                             <p className="home-card-title">{card.header}</p>
                             <p className="home-card-content">{card.body}</p>
                         </div>
                     ))}
                 </div>
                 <div id="signup-container" className="sticky-top">
-                    <h2>Sign up</h2>
-                    <form id="signup-form">
-                        <input type="email" placeholder="Email" required />
-                        <input type="password" placeholder="Password" required />
-                        <input type="password" placeholder="Confirm Password" required /><br></br>
-                        <input type="submit" value="Submit"/>
+                    <h3>Create an account</h3>
+                    <p>Start managing your finances by filling out the form below.</p>
+                    <form id="signup-form" onSubmit={handleSubmit}>
+                        <input 
+                            type="text" 
+                            placeholder="First Name" 
+                            value={firstName} 
+                            onChange={(e) => setFirstName(e.target.value)} 
+                            required 
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Last Name" 
+                            value={lastName} 
+                            onChange={(e) => setLastName(e.target.value)} 
+                            required 
+                        />
+                        <input 
+                            type="email" 
+                            placeholder="Email" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}  
+                            required 
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Password" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required 
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Confirm Password" 
+                            value={confirmPassword} 
+                            onChange={(e) => setConfirmPassword(e.target.value)} 
+                            required 
+                        /><br></br>
+                        <input 
+                            type="submit" 
+                            value="Submit"
+                        />
                     </form>
                 </div>
             </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 function CapitalizeFirst(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
+function brandEnter() {
+  const target = document.querySelector('.bi-wallet')
+
+  if (target) {
+    target.classList.remove('bi-wallet')
+    target.classList.add('bi-wallet2')
+  }
+}
+
+function brandLeave() {
+  const target = document.querySelector('.bi-wallet2')
+
+  if (target) {
+    target.classList.remove('bi-wallet2')
+    target.classList.add('bi-wallet')
+  }
+}
+
 function RenderNavbar() {
     const pills = ['features', 'about'];
 
     return (
-        <nav className="navbar sticky-top bg-body-tertiary">
-          <div className="container-fluid d-flex justify-content-between align-items-center">
-            <div className="d-flex align-items-center">
-              <Link className="navbar-brand" to="/">
-                <i className="bi bi-wallet2 d-inline-block align-text-top"></i>
-                WeGonBudget
-              </Link>
-              <ul className="nav nav-pills">
-                {pills.map((pill) => (
-                    <li className="nav-item"><Link className="nav-link" to={`/${pill}`}>{CapitalizeFirst(pill)}</Link></li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <Link className="nav-login" to="/login"><i className="bi bi-box-arrow-in-left login-arrow"></i>Log in</Link>
-            </div>
+      <nav className="navbar sticky-top bg-body-tertiary">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center">
+            <Link className="navbar-brand" to="/" onMouseEnter={brandEnter} onMouseLeave={brandLeave}>
+              <i className="bi-wallet d-inline-block align-text-top"></i>
+              WeGonBudget
+            </Link>
+            <ul className="nav nav-pills">
+              {pills.map((pill) => (
+                  <li className="nav-item"><Link className="nav-link" to={`/${pill}`}>{CapitalizeFirst(pill)}</Link></li>
+              ))}
+            </ul>
           </div>
-        </nav>
-      );
-            
+          <div>
+            <Link className="nav-login" to="/login"><i className="bi bi-box-arrow-in-left login-arrow"></i>Log in</Link>
+          </div>
+        </div>
+      </nav>
+    );      
 }
 
 
