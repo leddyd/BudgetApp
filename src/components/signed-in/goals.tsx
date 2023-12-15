104a7534e7e8fd18c13e53536fa5b62913745cfa
Dylan Leddy <leddyd@bc.edu>
2023-12-14 19:44:48 -0500

    Added beginning slider logic

--- src/components/signed-in/goals.tsx
+++ src/components/signed-in/goals.tsx
@@ -1,6 +1,68 @@
-
+import { useState } from "react";
 
 function RenderGoals() {
+    const [debtValue, setDebtValue] = useState(25);
+    const [savingsValue, setSavingsValue] = useState(25);
+    const [needsValue, setNeedsValue] = useState(25);
+    const [wantsValue, setWantsValue] = useState(25);
+
+    const recalculateSliders = (changedSlider: 'debt' | 'savings' | 'needs' | 'wants', newValue: number, oldValue: number) => {
+        let magnitude = (oldValue - newValue) / 3;
+    
+        switch (changedSlider) {
+            case 'debt':
+                setSavingsValue(Math.min(Math.max(savingsValue + magnitude, 0), 100));
+                setNeedsValue(Math.min(Math.max(needsValue + magnitude, 0), 100));
+                setWantsValue(Math.min(Math.max(wantsValue + magnitude, 0), 100));
+                break;
+            case 'savings':
+                setDebtValue(Math.min(Math.max(debtValue + magnitude, 0), 100));
+                setNeedsValue(Math.min(Math.max(needsValue + magnitude, 0), 100));
+                setWantsValue(Math.min(Math.max(wantsValue + magnitude, 0), 100));
+                break;
+            case 'needs':
+                setDebtValue(Math.min(Math.max(debtValue + magnitude, 0), 100));
+                setSavingsValue(Math.min(Math.max(savingsValue + magnitude, 0), 100));
+                setWantsValue(Math.min(Math.max(wantsValue + magnitude, 0), 100));
+                break;
+            case 'wants':
+                setDebtValue(Math.min(Math.max(debtValue + magnitude, 0), 100));
+                setNeedsValue(Math.min(Math.max(needsValue + magnitude, 0), 100));
+                setSavingsValue(Math.min(Math.max(savingsValue + magnitude, 0), 100));
+                break;
+            default:
+                break;
+            }
+        }
+    
+      const handleDebtChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const oldValue = debtValue;
+        const newValue = parseInt(e.target.value, 10);
+        setDebtValue(newValue);
+        recalculateSliders('debt', newValue, oldValue);
+      };
+    
+      const handleSavingsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const oldValue = savingsValue;
+        const newValue = parseInt(e.target.value, 10);
+        setSavingsValue(newValue);
+        recalculateSliders('savings', newValue, oldValue);
+      };
+    
+      const handleNeedsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const oldValue = needsValue;
+        const newValue = parseInt(e.target.value, 10);
+        setNeedsValue(newValue);
+        recalculateSliders('needs', newValue, oldValue);
+      };
+
+      const handleWantsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const oldValue = wantsValue;
+        const newValue = parseInt(e.target.value, 10);
+        setWantsValue(newValue);
+        recalculateSliders('wants', newValue, oldValue);
+      };
+
     return (
         <div className="app-page-container">
             <div className="header-container hidden">
@@ -14,20 +76,32 @@ function RenderGoals() {
                         </div>
                     </div>
                     <div className="card-content-wrapper">
-                        <label htmlFor="income-field" className="form-label">After-Tax Income</label>
+                        <label htmlFor="income-field" className="form-label">After-Tax Monthly Income</label>
                         <div className="input-group mb-3">
                             <span className="input-group-text">$</span>
                             <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)" id="income-field"/>
                             <span className="input-group-text">.00</span>
                         </div>
-                        <label htmlFor="debt-slider" className="form-label">Debt</label>
-                        <input type="range" className="form-range" id="debt-slider"></input>
-                        <label htmlFor="savings-slider" className="form-label">Savings</label>
-                        <input type="range" className="form-range" id="savings-slider"></input>
-                        <label htmlFor="needs-slider" className="form-label">Needs</label>
-                        <input type="range" className="form-range" id="needs-slider"></input>
-                        <label htmlFor="wants-slider" className="form-label">Wants</label>
-                        <input type="range" className="form-range" id="wants-slider"></input>
+                        <label htmlFor="debt-slider" className="form-label">
+                            Debt
+                            <span className="slider-value">{Math.trunc(debtValue)}%</span>
+                        </label>
+                        <input type="range" className="form-range" step="1" id="debt-slider" value={debtValue} onChange={handleDebtChange}></input>
+                        <label htmlFor="savings-slider" className="form-label">
+                            Savings
+                            <span className="slider-value">{Math.trunc(savingsValue)}%</span>
+                        </label>
+                        <input type="range" className="form-range" step="1" id="savings-slider" value={savingsValue} onChange={handleSavingsChange}></input>
+                        <label htmlFor="needs-slider" className="form-label">
+                            Needs
+                            <span className="slider-value">{Math.trunc(needsValue)}%</span>
+                        </label>
+                        <input type="range" className="form-range" step="1" id="needs-slider" value={needsValue} onChange={handleNeedsChange}></input>
+                        <label htmlFor="wants-slider" className="form-label">
+                            Wants
+                            <span className="slider-value">{Math.trunc(wantsValue)}%</span>
+                        </label>
+                        <input type="range" className="form-range" step="1" id="wants-slider" value={wantsValue} onChange={handleWantsChange}></input>
                     </div>
                 </div>
                 <div className="viz-card subscriptions hidden">
