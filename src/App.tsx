f9709801cd2eca60fbe5ea49b3ea62bafeff09aa
bprall <brprall3@gmail.com>
2023-12-22 03:37:36 -0600

    add firebase hosting, make login functional, add sign-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,82 @@
-import { Route, Routes } from "react-router-dom"
-import RenderAlert from "./components/signed-out/alert"
-import RenderNonUserNavbar from "./components/signed-out/nonuser-navbar"
-import RenderHome from "./components/signed-out/home"
-import React from "react"
-import RenderLogin from "./components/signed-out/login"
-import RenderUserNavbar from "./components/signed-in/user-navbar"
-import RenderExpenses from "./components/signed-in/expenses"
-import RenderGoals from "./components/signed-in/goals"
-import RenderAchievements from "./components/signed-in/achievements"
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import firebaseConfig from "../firebaseConfig";
+import RenderAlert from "./components/signed-out/alert";
+import RenderNonUserNavbar from "./components/signed-out/nonuser-navbar";
+import RenderHome from "./components/signed-out/home";
+import RenderLogin from "./components/signed-out/login";
+import RenderUserNavbar from "./components/signed-in/user-navbar";
+import RenderExpenses from "./components/signed-in/expenses";
+import RenderGoals from "./components/signed-in/goals";
+import RenderAchievements from "./components/signed-in/achievements";
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+      setUser(authUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Routes>
-        <Route path="/" element={
-          <div className="wrapper">
-            <RenderAlert />
-            <RenderNonUserNavbar />
-            <RenderHome />
-          </div>
-        } />
+        <Route
+          path="/"
+          element={
+            <div className="wrapper">
+              <RenderAlert />
+              <RenderNonUserNavbar />
+              <RenderHome />
+            </div>
+          }
+        />
         <Route path="/login" element={<RenderLogin />} />
-        <Route path="/profile" element={
-          <div className="main-container">
-            <RenderUserNavbar />
-          </div>
-        } />
-        <Route path="/expenses" element={
-          <div className="main-container">
-            <RenderUserNavbar />
-            <RenderExpenses />
-          </div>
-        } />
-        <Route path="/goals" element={
-          <div className="main-container">
-            <RenderUserNavbar />
-            <RenderGoals />
-          </div>
-        } />
-        <Route path="/achievements" element={
-          <div className="main-container">
-            <RenderUserNavbar />
-            <RenderAchievements />
-          </div>
-        } />
+        <Route
+          path="/profile"
+          element={
+            <div className="main-container">
+              {user ? <RenderUserNavbar /> : null}
+            </div>
+          }
+        />
+        <Route
+          path="/expenses"
+          element={
+            <div className="main-container">
+              {user ? <RenderUserNavbar /> : null}
+              <RenderExpenses />
+            </div>
+          }
+        />
+        <Route
+          path="/goals"
+          element={
+            <div className="main-container">
+              {user ? <RenderUserNavbar /> : null}
+              <RenderGoals />
+            </div>
+          }
+        />
+        <Route
+          path="/achievements"
+          element={
+            <div className="main-container">
+              {user ? <RenderUserNavbar /> : null}
+              <RenderAchievements />
+            </div>
+          }
+        />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/signed-out/login.tsx
+++ src/components/signed-out/login.tsx
@@ -1,58 +1,74 @@
-import React, { useEffect, useRef, useState } from "react"
-import { Link } from "react-router-dom"
-import { brandEnter, brandLeave } from "../../utils/mouseEvents"
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import firebaseConfig from '../../../firebaseConfig';  // Adjust the import path
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
 
 function RenderLogin() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate(); 
 
-    const handleSubmit = (e:any) => {
-        e.preventDefault();
-        console.log({email, password});
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-    return (
-        <>
-            <div className="top-bar"></div>
-            <div className="brand-container">
-                <i className="login-brand-icon bi-wallet d-inline-block align-text-top"></i>
-                <Link className="login-brand-link navbar-brand" to="/">WeGonBudget</Link>
-            </div>
-            <div className="login-form">
-                <form id="signup-form" onSubmit={handleSubmit}>
-                    <div className="form-floating mb-3">
-                        <input
-                            className="form-control" 
-                            type="email" 
-                            placeholder="Email" 
-                            id="floatingEmail"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}  
-                            required 
-                        />
-                        <label htmlFor="floatingEmail">Email</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input 
-                            className="form-control"
-                            id="floatingPassword"
-                            type="password" 
-                            placeholder="Password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
-                            required 
-                        />
-                        <label htmlFor="floatingPassword">Password</label>
-                    </div>
-                    <br></br>
-                    <input 
-                        type="submit" 
-                        value="Sign in"
-                    />
-                </form>
-            </div>
-        </>
-    )
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/profile");
+    } catch (error) {
+      console.error("Authentication error:", error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="top-bar"></div>
+      <div className="brand-container">
+        <i className="login-brand-icon bi-wallet d-inline-block align-text-top"></i>
+        <Link className="login-brand-link navbar-brand" to="/">WeGonBudget</Link>
+      </div>
+      <div className="login-form">
+        <form id="login-form" onSubmit={handleSubmit}>
+          <div className="form-floating mb-3">
+            <input
+              className="form-control"
+              type="email"
+              placeholder="Email"
+              id="floatingEmail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="floatingEmail">Email</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input
+              className="form-control"
+              id="floatingPassword"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+          <br></br>
+          <input
+            type="submit"
+            value="Sign in"
+          />
+        </form>
+        <p>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default RenderLogin
+export default RenderLogin;
