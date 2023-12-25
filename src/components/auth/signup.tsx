1085e17197e45c811a9c8cce6cc0bfcb8fe47bba
bprall <brprall3@gmail.com>
2023-12-24 18:01:15 -0600

    implement firestore and save user first and last name to db on signup

--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,107 +1,131 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, } from 'firebase/auth';
+import { addDoc, collection } from "firebase/firestore"; 
+import { getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithPopup, 
+  GoogleAuthProvider, } from 'firebase/auth';
+import { db } from '../../../firebaseConfig.ts';
 
-export function SignUp() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const navigate = useNavigate();
-  const auth = getAuth();
-
-  const fields = [
-    {
-      id: "floatingFirstName", 
-      type: "text", placeholder: "First Name", 
-      value: firstName, 
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)
-    },
-    {
-      id: "floatingLastName", 
-      type: "text", placeholder: "Last Name", 
-      value: lastName, 
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)
-    },
-    {
-      id: "floatingEmail", 
-      type: "email", placeholder: "Email", 
-      value: email, 
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
-    },
-    {
-      id: "floatingPassword", 
-      type: "password", 
-      placeholder: "Password", 
-      value: password, 
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
-    },
-    {
-      id: "floatingConfirmPassword", 
-      type: "password", 
-      placeholder: "Confirm Password", 
-      value: confirmPassword, 
-      onChange: (e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)
-    },
-  ]
-
-  const onSignUpSuccess = () => {
-    navigate('/profile', {replace: true});
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (password !== confirmPassword) {
-      console.error('Passwords do not match');
-      return;
-    }
-
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
+  export function SignUp() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
+    const auth = getAuth();
+  
+    const fields = [
+      {
+        id: "floatingFirstName", 
+        type: "text", placeholder: "First Name", 
+        value: firstName, 
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => setFirstName(e.target.value)
+      },
+      {
+        id: "floatingLastName", 
+        type: "text", placeholder: "Last Name", 
+        value: lastName, 
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => setLastName(e.target.value)
+      },
+      {
+        id: "floatingEmail", 
+        type: "email", placeholder: "Email", 
+        value: email, 
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
+      },
+      {
+        id: "floatingPassword", 
+        type: "password", 
+        placeholder: "Password", 
+        value: password, 
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
+      },
+      {
+        id: "floatingConfirmPassword", 
+        type: "password", 
+        placeholder: "Confirm Password", 
+        value: confirmPassword, 
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)
+      },
+    ];
+  
+    const onSignUpSuccess = async () => {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+  
+        const docRef = await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          first: firstName,
+          last: lastName,
+          email: user.email,
+        });
+  
+        console.log("Document written with ID: ", docRef.id);
+        
+        navigate('/profile', { replace: true });
+      } catch (error) {
+        console.error('Registration error:', error.message);
+      }
+    };
+  
+    const handleGoogleSignUp = async () => {
+      const provider = new GoogleAuthProvider();
+      try {
+        const result = await signInWithPopup(auth, provider);
+        const user = result.user;
+  
+        const docRef = await addDoc(collection(db, "users"), {
+          uid: user.uid,
+          first: user.displayName,
+          email: user.email,
+        });
+  
+        console.log("Document written with ID: ", docRef.id);
+  
+        navigate('/profile', { replace: true });
+      } catch (error) {
+        console.error('Google sign-up error:', error.message);
+      }
+    };
+  
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+  
+      if (password !== confirmPassword) {
+        console.error('Passwords do not match');
+        return;
+      }
       onSignUpSuccess();
-    } catch (error) {
-      console.error('Registration error:', error.message);
-    }
-  };
-
-  const handleGoogleSignUp = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      onSignUpSuccess();
-    } catch (error) {
-      console.error('Google sign-up error:', error.message);
-    }
-  };
-
-  return (
-    <form id="signup-form" onSubmit={handleSubmit}>
-      {fields.map((field) => (
-        <div className="form-floating mb-3">
-          <input
-            className="form-control"
-            type={field.type}
-            placeholder={field.placeholder}
-            id={field.id}
-            value={field.value}
-            onChange={field.onChange}
-            required
-          />
-          <label htmlFor={field.id}>{field.placeholder}</label>
-        </div>
-      ))}
-      <br />
-      <input type="submit" value="Sign Up" />
-      <button type="button" className="google-btn" onClick={handleGoogleSignUp}>
-        <img className="google-icon" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="Google Icon" />
-        Sign Up with Google
-      </button>
-    </form>
-  );
-}
+    };
+  
+    return (
+      <form id="signup-form" onSubmit={handleSubmit}>
+        {fields.map((field) => (
+          <div className="form-floating mb-3" key={field.id}>
+            <input
+              className="form-control"
+              type={field.type}
+              placeholder={field.placeholder}
+              id={field.id}
+              value={field.value}
+              onChange={field.onChange}
+              required
+            />
+            <label htmlFor={field.id}>{field.placeholder}</label>
+          </div>
+        ))}
+        <br />
+        <input type="submit" value="Sign Up" />
+        <button type="button" className="google-btn" onClick={handleGoogleSignUp}>
+          <img className="google-icon" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" alt="Google Icon" />
+          Sign Up with Google
+        </button>
+      </form>
+    );
+  }
 
 function RenderSignUp() {
   return (
