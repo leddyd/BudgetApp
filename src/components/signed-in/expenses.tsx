29a0b69f64961aa43a2ee377c5486dad872979f1
bprall <brprall3@gmail.com>
2023-12-27 19:53:42 -0600

    create functions to fetch subscription and tansaction data from db, integrate into expenses page

--- src/components/signed-in/expenses.tsx
+++ src/components/signed-in/expenses.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import PieChart from '../charts/expensePieChart';
 import ProgressBar from '../charts/progressBar';
 import AddTransactionModal from '../modals/add-transaction';
 import AddSubscriptionModal from '../modals/add-subscription';
 import { getAllMonths, getCurrentMonth } from '../../utils/dateUtils';
 import WantsMeter from '../charts/wantsMeter';
+import { collection, getDocs, query, DocumentData } from 'firebase/firestore';
+import { db, auth } from '../../../firebaseConfig.ts';
+
 
 interface Datum {
     label: string;
@@ -17,38 +20,79 @@ const data: Datum[] = [
     { label: 'Restaurants', value: 20 },
 ];
 
-var transactions = [
-    {party: "Dave", date: "12/22", type: "Received", amount:"$50"},
-    {party: "Wiseman", date: "12/22", type: "Received", amount:"$50"},
-    {party: "Steve", date: "12/22", type: "Sent", amount:"$50"},
-    {party: "Bob", date: "12/22", type: "Sent", amount:"$50"},
-    {party: "Bill", date: "12/22", type: "Received", amount:"$50"},
-];
+export const fetchTransactions = async () => {
+    try {
 
-var subscriptions = [
-    {party: "Netflix", date: "Since 8/03", amount:"$10"},
-    {party: "Brazzers", date: "Since 8/03", amount:"$12"},
-    {party: "Spotify", date: "Since 8/03", amount:"$5"},
-    {party: "Xbox", date: "Since 8/03", amount:"$5"},
-    {party: "Amazon Prime", date: "Since 8/03", amount:"$15"},
-];
+      const userId = auth.currentUser?.uid;
 
-function RenderExpenses() {
+      const transactionsCollection = collection(db, `users/${userId}/transactions`);
+      const transactionsQuery = query(transactionsCollection);
+  
+      const transactionsSnapshot = await getDocs(transactionsQuery);
+      const transactionsData = transactionsSnapshot.docs.map((doc) => doc.data());
+  
+      return transactionsData;
+    } catch (error) {
+      console.error('Error fetching transactions:', error.message);
+      return [];
+    }
+  };
+  
+  export const fetchSubscriptions = async () => {
+    try {
+
+      const userId = auth.currentUser?.uid;
+
+      const subscriptionsCollection = collection(db, `users/${userId}/subscriptions`);
+      const subscriptionsQuery = query(subscriptionsCollection);
+  
+      const subscriptionsSnapshot = await getDocs(subscriptionsQuery);
+      const subscriptionsData = subscriptionsSnapshot.docs.map((doc) => doc.data());
+  
+      return subscriptionsData;
+    } catch (error) {
+      console.error('Error fetching subscriptions:', error.message);
+      return [];
+    }
+  };
+
+  function RenderExpenses() {
     const [showTransactionModal, setShowTransactionModal] = useState(false);
     const [showSubscriptionModal, setShowSubscriptionModal] = useState(false);
     const [displayMonth, setDisplayMonth] = useState(getCurrentMonth);
-
+    const [transactions, setTransactions] = useState<DocumentData[]>([]);
+    const [subscriptions, setSubscriptions] = useState<DocumentData[]>([]);
+  
     const toggleSubscriptionModal = () => {
-        setShowSubscriptionModal(!showSubscriptionModal);
-    }
-
-    const toggleTransactionModal = () => {
-        setShowTransactionModal(!showTransactionModal);
+      setShowSubscriptionModal(!showSubscriptionModal);
     };
+  
+    const toggleTransactionModal = () => {
+      setShowTransactionModal(!showTransactionModal);
+    };
+  
+    const handleMonthChange = (newMonth: string) => {
+      setDisplayMonth(newMonth);
+    };
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const transactionsData = await fetchTransactions();
+          const subscriptionsData = await fetchSubscriptions();
 
-    const handleMonthChange = (newMonth:string) => {
-        setDisplayMonth(newMonth);
-    }
+         console.log('Transactions Data:', transactionsData);
+          console.log('Subscriptions Data:', subscriptionsData);
+
+          setTransactions(transactionsData);
+          setSubscriptions(subscriptionsData);
+       } catch (error) {
+         console.error('Error fetching data:', error.message);
+        }
+      };
+  
+      fetchData();
+    }, []);
 
     return (
         <div className="app-page-container">
@@ -106,32 +150,32 @@ function RenderExpenses() {
                     </div>
                 </div>
                 <div className="viz-card expenses hidden">
-                    <div className='navbar navbar-expand-lg bg-body-tertiary rounded-top-3'>
-                        <div className="container-fluid">
-                            <p className="mb-0 fs-5 text-body-emphasis fw-medium text-muted">Your Transactions</p>
-                            <button type="button" className="btn add-goal-btn" onClick={() => {setShowTransactionModal(!showTransactionModal)}}>
-                                <i className="bi bi-plus"></i>
-                            </button>
-                            {showTransactionModal && <AddTransactionModal onClose={toggleTransactionModal} />}
-                        </div>
+                  <div className='navbar navbar-expand-lg bg-body-tertiary rounded-top-3'>
+                    <div className="container-fluid">
+                      <p className="mb-0 fs-5 text-body-emphasis fw-medium text-muted">Your Transactions</p>
+                      <button type="button" className="btn add-goal-btn" onClick={toggleTransactionModal}>
+                        <i className="bi bi-plus"></i>
+                      </button>
+                      {showTransactionModal && <AddTransactionModal onClose={toggleTransactionModal} />}
                     </div>
-                    <div className='expenses-list-group-container'>
-                        <ol className="list-group">
-                            {transactions.map((t, index) => (
-                                <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-                                    <div className="expense-category-indicator"></div>
-                                    <div className="ms-2 me-auto small">
-                                        <div className="fw-bold">{t.party}</div>
-                                        {t.date}
-                                    </div>
-                                    <span className={`badge ${t.type} rounded-pill`}>
-                                        <i className={t.type === "Received" ? "bi bi-arrow-down-left-circle-fill" : "bi bi-arrow-up-right-circle-fill"}></i>
-                                        {t.type} {t.amount}
-                                    </span>
-                                </li>
-                            ))}
-                        </ol>
-                    </div>
+                  </div>
+                  <div className='expenses-list-group-container'>
+                    <ol className="list-group">
+                      {transactions.map((t, index) => (
+                        <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                          <div className="expense-category-indicator"></div>
+                          <div className="ms-2 me-auto small">
+                            <div className="fw-bold">{t.note}</div>
+                            {t.createdAt && t.createdAt.toDate().toLocaleDateString()}
+                          </div>
+                          <span className={`badge ${t.transactionType} rounded-pill`}>
+                            <i className={t.transactionType === "Received" ? "bi bi-arrow-down-left-circle-fill" : "bi bi-arrow-up-right-circle-fill"}></i>
+                           {t.transactionType} ${t.amount.toFixed(2)}
+                         </span>
+                        </li>
+                      ))}
+                   </ol>
+                 </div>
                 </div>
                 <div className="viz-card subscriptions hidden">
                     <div className='navbar navbar-expand-lg bg-body-tertiary rounded-top-3'>
@@ -149,8 +193,8 @@ function RenderExpenses() {
                                 <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
                                     <div className="expense-category-indicator"></div>
                                     <div className="ms-2 me-auto small">
-                                        <div className="fw-bold">{s.party}</div>
-                                        {s.date}
+                                        <div className="fw-bold">{s.note}</div>
+                                        {s.createdAt && s.createdAt.toDate().toLocaleDateString()}
                                     </div>
                                     <span className={"badge Sent rounded-pill"}>
                                         <i className="bi bi-arrow-up-right-circle-fill"></i>
